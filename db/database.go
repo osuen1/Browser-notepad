e653bc56d2b8e210299ab96405ef525ed7eea4f8
osuen1 <st.akimov1@gmail.com>
2026-02-08 22:46:22 +0300

    Пофиксил лазейку для sql инъекций

--- db/database.go
+++ db/database.go
@@ -192,7 +192,7 @@ func Update_token(pool *pgxpool.Pool, user_id string, token string) error {
 }
 
 func Get_token(pool *pgxpool.Pool, email string) (token string, err error) {
-	row := pool.QueryRow(context.Background(), "SELECT token FROM users WHERE email = '$1'", email)
+	row := pool.QueryRow(context.Background(), "SELECT token FROM users WHERE email = $1", email)
 	if err := row.Scan(&token); err != nil {
 		if err == pgx.ErrNoRows {
 			return "", err
